4f71d5487fff7d679bf093082fc5299c08acf2d0
João <joaorb64@gmail.com>
2020-01-15 11:14:31 -0300

    Updates

---
+++ precache-manifest.3be7fb2475a838c0b5899464026578fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1ac31ea773069bb535d77c7db7475e",
+    "url": "/smashrankingsbrazil/index.html"
+  },
+  {
+    "revision": "d7f6b2565d7dcd3d80d1",
+    "url": "/smashrankingsbrazil/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cffa851faf3905db930a",
+    "url": "/smashrankingsbrazil/static/js/2.44f38f18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/smashrankingsbrazil/static/js/2.44f38f18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7f6b2565d7dcd3d80d1",
+    "url": "/smashrankingsbrazil/static/js/main.eab448bd.chunk.js"
+  },
+  {
+    "revision": "1899b3e8c66be422c54c",
+    "url": "/smashrankingsbrazil/static/js/runtime-main.0a4351a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smashrankingsbrazil/static/media/logo.5d5d9eef.svg"
+  }
+]);
