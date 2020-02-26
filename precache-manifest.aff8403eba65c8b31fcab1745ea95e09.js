a5f213b04e007e5f8073a632736750acbc7b9dc2
João <joaorb64@gmail.com>
2020-02-26 15:36:11 -0300

    Updates

---
+++ precache-manifest.aff8403eba65c8b31fcab1745ea95e09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514853e2a083b0deef94f7bc2d03ab13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d47d0d1f7f0a6cd8a6a0",
+    "url": "/static/css/main.559a6875.chunk.css"
+  },
+  {
+    "revision": "36edec70144a79701aec",
+    "url": "/static/js/2.d53ed0ea.chunk.js"
+  },
+  {
+    "revision": "ce3126c332c3bc8f1544c88c174f7c51",
+    "url": "/static/js/2.d53ed0ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d47d0d1f7f0a6cd8a6a0",
+    "url": "/static/js/main.5a742ebb.chunk.js"
+  },
+  {
+    "revision": "0d7804f3076034bbd9e1",
+    "url": "/static/js/runtime-main.b9683416.js"
+  },
+  {
+    "revision": "98d757538489db0780e2941037c31396",
+    "url": "/static/media/Gotham Black Regular.98d75753.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71d5371f8729b84bf139338ef0f807a0.js"
+  "/precache-manifest.aff8403eba65c8b31fcab1745ea95e09.js"
 );
 
 self.addEventListener('message', (event) => {
