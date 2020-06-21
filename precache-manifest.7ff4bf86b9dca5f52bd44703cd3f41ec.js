11254d8644285f32a7f54c022d5e0d5787ba313e
João <joaorb64@gmail.com>
2020-06-21 12:21:45 -0300

    Updates

--- precache-manifest.7ff4bf86b9dca5f52bd44703cd3f41ec.js
+++ precache-manifest.7ff4bf86b9dca5f52bd44703cd3f41ec.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bd0241ddcacd4efb8afe86d47d89d4f2",
+    "revision": "32b3e0f32306d23f707220b1a8aaa238",
     "url": "/index.html"
   },
   {
-    "revision": "07a18c1c6933ff296baf",
-    "url": "/static/css/main.c5572bcd.chunk.css"
+    "revision": "d57c9e6d06ee79d2a226",
+    "url": "/static/css/main.a1cdcb10.chunk.css"
   },
   {
-    "revision": "287db8a6c03c1fdea287",
-    "url": "/static/js/2.e52ee00c.chunk.js"
+    "revision": "46507a303cd81b773b57",
+    "url": "/static/js/2.05de8e0c.chunk.js"
   },
   {
     "revision": "35ced010c5f50ba31d33feebc9913fa1",
-    "url": "/static/js/2.e52ee00c.chunk.js.LICENSE"
+    "url": "/static/js/2.05de8e0c.chunk.js.LICENSE"
   },
   {
-    "revision": "07a18c1c6933ff296baf",
-    "url": "/static/js/main.7a491f3b.chunk.js"
+    "revision": "d57c9e6d06ee79d2a226",
+    "url": "/static/js/main.08f40f81.chunk.js"
   },
   {
     "revision": "0d7804f3076034bbd9e1",
